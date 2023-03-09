968ed38e4084088f4018ec6bcff735b40159156b
shebl <shebblloll@gmail.com>
2023-03-09 14:27:00 +0200

    String repeat Problem1

---
+++ problem1-String repeat/main.js
@@ -0,0 +1,6 @@
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+function repeatStr(n, s) {
+  console.log(s.repeat(n));
+}
+repeatStr(2, "I");
